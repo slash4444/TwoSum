d88fb022b6a3d126cf21fd1188546fd0b29a3e0b
Bruno Lima <112622382+slash4444@users.noreply.github.com>
2023-09-26 09:20:21 +0100

    Add files via upload

---
+++ TwoSumEx/TwoSumDemonstration/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = TwoSum(nums, target);
+
+        if (result != null)
+        {
+            Console.WriteLine($"Indices of the two numbers that add up to {target}: {result[0]} and {result[1]}");
+        }
+        else
+        {
+            Console.WriteLine("No solution found.");
+        }
+    }
+
+    static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (numIndices.ContainsKey(complement))
+            {
+                return new int[] { numIndices[complement], i };
+            }
+
+            // Store the current number and its index in the map
+            // Armazena o número atual e o índice no map
+
+            numIndices[nums[i]] = i;
+        }
+
+        // No solution found
+        // Retorna alguma coisa como null
+        return null;
+    }
+}
